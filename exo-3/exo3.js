ad005f86ede2791d8e80677f823740c3347a6660
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-02 11:49:55 +0200

    Exercice3 - Les fonctions

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,77 @@
-// you can write js here
-console.log('exo-3');
+var playerInput = prompt("Your choice (rock, paper, scissors) ?");
+
+function getPlayerChoice(playerInput) {
+  playerInput = playerInput.toLowerCase();
+
+  if (
+    playerInput == "rock" ||
+    playerInput == "paper" ||
+    playerInput == "scissors" ||
+    playerInput == "bomb"
+  ) {
+    return playerInput;
+  } else {
+    throw new Error("Input is neither rock nor paper nor scissors.");
+  }
+}
+
+try {
+  getPlayerChoice(playerInput);
+} catch (e) {
+  console.error(e);
+}
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  switch (computerChoice) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+function findWinner(playerChoice, computerChoice) {
+  let result = "";
+
+  if (playerChoice == "bomb") {
+    return (result = "Win (but it feels like cheating, no ?");
+  }
+
+  if (playerChoice == computerChoice) {
+    return (result = "Tied");
+  } else if (playerChoice == "rock") {
+    if (computerChoice == "scissors") {
+      return (result = "Win");
+    } else {
+      return (result = "Lost");
+    }
+  } else if (playerChoice == "paper") {
+    if (computerChoice == "rock") {
+      return (result = "Win");
+    } else {
+      return (result = "Lost");
+    }
+  } else if (playerChoice == "scissors") {
+    if (computerChoice == "rock") {
+      return (result = "Lost");
+    } else {
+      return (result = "Win");
+    }
+  } else return (result = "The developer has lost");
+}
+
+function playGame() {
+  const uChoice = getPlayerChoice(playerInput);
+  const comChoice = getComputerChoice();
+  console.log(uChoice);
+  console.log(comChoice);
+  console.log(findWinner(uChoice, comChoice));
+}
+
+playGame();
