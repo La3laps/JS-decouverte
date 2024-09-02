d0fe5aca44af66fa73ffead5fce1d37802fafa56
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-02 11:18:02 +0200

    Exercice2 - Les conditions

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,25 @@
-// you can write js here
-console.log('exo-2');
+const myDate = new Date();
+
+const weekAlert = "Nous sommes en semaine.";
+const weekendAlert = "Nous sommes le weekend!";
+
+const isTesting = true;
+
+let today;
+let hour;
+
+if (isTesting) {
+  today = prompt("Quel jour sommes nous ? (chiffre)");
+  hour = prompt("Quelle heure ?");
+} else {
+  today = myDate.getDay();
+  hour = myDate.getHours();
+}
+
+if ((today == 5 && hour >= 17) || (today == 1 && hour < 9)) {
+  console.log(weekendAlert);
+} else if (today != 0 && today < 6) {
+  console.log(weekAlert);
+} else {
+  console.log(weekendAlert);
+}
