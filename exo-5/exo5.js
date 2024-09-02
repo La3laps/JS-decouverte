ed88ac5997098e4582cbe07d284d02e750a8e4a5
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-02 13:37:44 +0200

    Exercice5 - Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,15 @@
-// you can write js here
+const input = prompt("Message à traduire en baleine.").toLowerCase();
 
-console.log('exo-5');
+const vowels = ["a", "e", "i", "o", "u"];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  let findVowel = vowels.indexOf(input[i]);
+
+  if (findVowel != -1) {
+    let vowel = vowels[findVowel].toUpperCase();
+    resultArray.push(vowel);
+  }
+}
+console.log(resultArray.join(""));
