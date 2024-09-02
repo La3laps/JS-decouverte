8f1f3b2ce152abd110d6b50b9ee5c4f25d869435
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-02 12:29:35 +0200

    Exercice4 - Les tableaux

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,30 @@
-// you can write js here
+var secretMessage = [
+  "Learning",
+  "isn't",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it's",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
 
-console.log('exo-4');
-
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+secretMessage.pop();
+secretMessage.shift();
+secretMessage.unshift("Programming");
+secretMessage.splice(5, 5, "know");
+console.log(secretMessage.join(" "));
