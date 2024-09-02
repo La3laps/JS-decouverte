453186be575463b0eeb9d0c729d7fb784baccef1
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-02 10:50:34 +0200

    Exercice1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,16 @@
 // you can write js here
-console.log('hello from file');
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+let celcius = kelvins - 273;
+
+let fahrenheits = celcius * (9/5) + 32;
+fahrenheits = Math.floor(fahrenheits);
+// Ici on convertit les celcius en fahrenheit et on utilise la methode
+// .floor() pour arrondir à l'entier inférieur
+
+console.log("La température en celcius est de " + celcius);
+console.log("La température en fahrenheit est de " + fahrenheits);
+
+
+
+
